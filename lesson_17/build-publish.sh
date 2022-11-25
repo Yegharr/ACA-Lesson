dcf2c90deba7e02b20c059e6c8e526667b5312d3
yegharr <yeghishe.harutyunyan@gmail.com>
2022-11-25 21:03:48 +0400

    lesson_17 task

--- lesson_17/build-publish.sh
+++ lesson_17/build-publish.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
-#if [("$1","$2", "$3" "$4" ,"$5") = true ] ;then
 
-docker login --username="$1" --password="$2"
-docker build -t ubuntu20."$3"  "$4" 
-docker tag ubuntu20.04:"$3"  "$5"/ubuntu:20.04_"$3"
-docker push yegharr/ubuntu:"$3"
 
-#$1=username 
-#$2=password 
-#$3=version
-#$4= Dockerfile path 
-#$5=image repo name 
+
+
+          docker login -u $1 -p $2
+	  docker build -t $3 $4 
+	  docker push $3   
+	  
+#username = $1
+#password = $2
+#image_repository_name =$3
+#dockerfile path = $4
+#image tag= has been included in $3
+
+
 
